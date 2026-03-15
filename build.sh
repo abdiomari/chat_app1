8e40ba2e62436d40ad1fde9d8d555ebfac118725
Abdi Omari <abdikipkorir@gmail.com>
2026-03-15 17:49:52 +0300

    deployment configs

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@ set -e
 
 pip install -r requirements.txt
 
+npm install -D tailwindcss
+npx tailwindcss -i ./static/css/input.css -o ./static/css/output.css --minify
+
 python manage.py collectstatic --noinput
 
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './chat/templates/**/*.html',
+    './accounts/templates/**/*.html',
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
