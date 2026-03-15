3ee687e354a3d14d5700e24e326dedf353118136
Abdi Omari <abdikipkorir@gmail.com>
2026-03-15 18:03:23 +0300

    deployment configs

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 set -e
-
 pip install -r requirements.txt
-
-npm install -D tailwindcss
-npx tailwindcss -i ./static/css/input.css -o ./static/css/output.css --minify
-
 python manage.py collectstatic --noinput
-
